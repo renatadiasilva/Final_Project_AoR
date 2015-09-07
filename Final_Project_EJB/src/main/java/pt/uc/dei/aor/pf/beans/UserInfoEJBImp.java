d68d5266b0be9ebd410c5675580323ccb9e2364b
renatadiasilva <renatadiasilva@gmail.com>
2015-09-07 19:51:53 +0100

    user info

--- Final_Project_EJB/src/main/java/pt/uc/dei/aor/pf/beans/UserInfoEJBImp.java
+++ Final_Project_EJB/src/main/java/pt/uc/dei/aor/pf/beans/UserInfoEJBImp.java
@@ -1,5 +1,81 @@
 package pt.uc.dei.aor.pf.beans;
 
-public class UserInfoEJBImp {
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import pt.uc.dei.aor.pf.entities.PositionEntity;
+import pt.uc.dei.aor.pf.entities.UserInfoEntity;
+
+@Stateless
+public class UserInfoEJBImp implements UserInfoEJBInterface {
+
+	@Override
+	public void save(UserInfoEntity user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(UserInfoEntity user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(UserInfoEntity user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByCity(String city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByPhone(String phone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByMobile(String mobilePhone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByCountry(String country) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesByCourse(String course) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidatesBySchool(String school) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserInfoEntity> findCandidates(String address, String city,
+			String phone, String mobilePhone, String country, String course,
+			String school, PositionEntity position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
