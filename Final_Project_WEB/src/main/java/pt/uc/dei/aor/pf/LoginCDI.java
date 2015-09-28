960032f62cae0b92e574c43f503077a5e389f982
renatadiasilva <renatadiasilva@gmail.com>
2015-09-28 17:31:33 +0100

    errito loggad already

--- Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/LoginCDI.java
+++ Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/LoginCDI.java
@@ -1,14 +1,10 @@
 package pt.uc.dei.aor.pf;
 
-import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import pt.uc.dei.aor.pf.session.UserSessionManagement;
-import pt.uc.dei.aor.pf.webManagement.UserManagementInterface;
 
 @Named
 @RequestScoped
@@ -16,9 +12,6 @@ public class LoginCDI {
 
 	@Inject
 	private UserSessionManagement userSessionManagement;
-
-	@EJB
-	UserManagementInterface userManagement;
 
 	private String email;
 
@@ -28,13 +21,7 @@ public class LoginCDI {
 	}
 
 	public void login() {
-		// Verifica se já existe um user logado
-		if(!this.userManagement.isUserLogged()){
-			this.userSessionManagement.login(this.email, this.password);
-		} else 
-			FacesContext.getCurrentInstance().addMessage(null, 
-					new FacesMessage(FacesMessage.SEVERITY_WARN,
-							"Já existe um user logado", ""));
+		this.userSessionManagement.login(this.email, this.password);
 	}
 
 	public String getEmail() {
