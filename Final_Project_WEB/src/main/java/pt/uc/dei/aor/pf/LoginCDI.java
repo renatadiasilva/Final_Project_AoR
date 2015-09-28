75e91bd8c861b2fe8d676bb61178a15adc2126ad
renatadiasilva <renatadiasilva@gmail.com>
2015-09-28 18:01:50 +0100

    erro user logado na mesma sessão

--- Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/LoginCDI.java
+++ Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/LoginCDI.java
@@ -4,11 +4,17 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import pt.uc.dei.aor.pf.session.UserSessionManagement;
 
 @Named
 @RequestScoped
 public class LoginCDI {
+
+	private static final Logger log = 
+			LoggerFactory.getLogger(LoginCDI.class);
 
 	@Inject
 	private UserSessionManagement userSessionManagement;
@@ -21,7 +27,11 @@ public class LoginCDI {
 	}
 
 	public void login() {
-		this.userSessionManagement.login(this.email, this.password);
+		try {
+			this.userSessionManagement.login(this.email, this.password);
+		} catch (IllegalStateException e) {
+			log.error("Já há um user logado na mesma sessão");
+		}
 	}
 
 	public String getEmail() {
