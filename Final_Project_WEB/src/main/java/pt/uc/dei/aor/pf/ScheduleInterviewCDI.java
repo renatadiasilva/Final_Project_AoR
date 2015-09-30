4d169d0d8b800288cb8bb892df0b2f7078019f78
DuarteGoncalves <duarte.m.a.goncalves@gmail.com>
2015-09-30 21:05:42 +0100

    Gestor -> Agendar Entrevista

--- Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/ScheduleInterviewCDI.java
+++ Final_Project_WEB/src/main/java/pt/uc/dei/aor/pf/ScheduleInterviewCDI.java
@@ -7,18 +7,25 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import pt.uc.dei.aor.pf.beans.InterviewEJBInterface;
+import pt.uc.dei.aor.pf.beans.PositionEJBInterface;
+import pt.uc.dei.aor.pf.beans.ScriptEJBInterface;
 import pt.uc.dei.aor.pf.beans.SubmissionEJBInterface;
 import pt.uc.dei.aor.pf.beans.UserEJBInterface;
 import pt.uc.dei.aor.pf.constants.Constants;
 import pt.uc.dei.aor.pf.entities.InterviewEntity;
 import pt.uc.dei.aor.pf.entities.PositionEntity;
+import pt.uc.dei.aor.pf.entities.ScriptEntity;
 import pt.uc.dei.aor.pf.entities.SubmissionEntity;
 import pt.uc.dei.aor.pf.entities.UserEntity;
 import pt.uc.dei.aor.pf.session.UserSessionManagement;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +37,8 @@ public class ScheduleInterviewCDI implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -1929597378229479373L;
+
+	private static final Logger log = LoggerFactory.getLogger(ScheduleInterviewCDI.class);
 
 	@Inject
 	private UserSessionManagement userManagement;
@@ -43,6 +52,14 @@ public class ScheduleInterviewCDI implements Serializable {
 	@EJB
 	private InterviewEJBInterface interviewEJB;
 
+	@EJB
+	private ScriptEJBInterface scriptEJB;
+
+	@EJB
+	private PositionEJBInterface positionEJB;
+
+	private List<PositionEntity>positions;
+
 	private List<UserEntity>interviewers, selectedInterviewers;
 
 	private List<SubmissionEntity>submissions;
@@ -54,6 +71,14 @@ public class ScheduleInterviewCDI implements Serializable {
 	private List<InterviewEntity> interviews;
 
 	private Date interviewDate;
+
+	private List<ScriptEntity> scripts;
+
+	private ScriptEntity selectedScript;
+
+	private List<String>conflicts;
+
+	private String keyword;
 
 	public List<SubmissionEntity> getSubmissions() {
 
@@ -85,6 +110,7 @@ public class ScheduleInterviewCDI implements Serializable {
 
 	public void unloadPosition(){
 		this.position=null;
+		this.cleanBean();
 	}
 
 	public void loadSubmission(SubmissionEntity submission){
@@ -95,6 +121,8 @@ public class ScheduleInterviewCDI implements Serializable {
 
 	public void unloadSubmission(){
 		this.submission=null;
+		this.interviewDate=null;
+		this.selectedScript=null;
 	}
 
 	public boolean loadedPosition(){
@@ -149,17 +177,24 @@ public class ScheduleInterviewCDI implements Serializable {
 	}
 
 	public Date minDate(){
-		return new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.HOUR_OF_DAY, 1);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+
+		return cal.getTime();
 	}
-	
+
 	public Date getInterviewDate() {
 		return interviewDate;
 	}
 
 	public void setInterviewDate(Date interviewDate) {
-		// FDP passou-se
-		this.interviewDate=new Date();
-//		this.interviewDate = interviewDate;
+		this.interviewDate=interviewDate;
+	}
+
+	public boolean hasDate(){
+		return this.interviewDate!=null;
 	}
 
 	public void buildInterviewersList(){
@@ -214,9 +249,12 @@ public class ScheduleInterviewCDI implements Serializable {
 	}
 
 	public void addInterviewer(UserEntity interviewer){
+		if(this.scripts==null||this.scripts.isEmpty())
+			this.scripts=this.scriptEJB.findReusableScripts();
 		if(this.selectedInterviewers==null)
 			this.selectedInterviewers=new ArrayList<UserEntity>();
 		this.selectedInterviewers.add(interviewer);
+		this.buildConflicts();
 	}
 
 	public void removeInterviewer(UserEntity interviewer){
@@ -229,13 +267,14 @@ public class ScheduleInterviewCDI implements Serializable {
 
 		return this.selectedInterviewers.contains(interviewer);
 	}
-	
+
 	public void createInterview(){
-		System.out.println("Create Interview!");
+		if(this.selectedScript==null)this.selectedScript=this.position.getDefaultScript();
+
 		if(this.selectedInterviewers==null||this.selectedInterviewers.isEmpty())
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Escolha o(s) entrevistador(es)", ""));
 		else{
-			InterviewEntity newInterview=new InterviewEntity(this.submission, this.interviewDate, this.position.getDefaultScript(), this.userEJB.findUserByEmail(this.userManagement.getUserMail()));
+			InterviewEntity newInterview=new InterviewEntity(this.submission, this.interviewDate, this.selectedScript, this.userEJB.findUserByEmail(this.userManagement.getUserMail()));
 			newInterview.setInterviewers(this.selectedInterviewers);
 			this.interviewEJB.save(newInterview);
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Nova entrevista agendada."));
@@ -243,19 +282,136 @@ public class ScheduleInterviewCDI implements Serializable {
 		}
 	}
 
-	private void cleanBean() {
-		this.interviewers.clear();
-		
-		this.selectedInterviewers.clear();;
+	public boolean hasSelectedInterviewers(){
+		if(this.selectedInterviewers==null)
+			return false;
+		return !this.selectedInterviewers.isEmpty();
+	}
 
-		this.submissions.clear();;
+	//	public boolean hasScripts(){
+	//		if(this.scripts==null)
+	//			this.scripts=this.scriptEJB.findReusableScripts();			
+	//		
+	//		return this.scripts!=null&&!this.scripts.isEmpty();
+	//	}
+
+	public List<ScriptEntity> getScripts() {
+		return scripts;
+	}
+
+	public void setScripts(List<ScriptEntity> scripts) {
+		this.scripts = scripts;
+	}
+
+	public void loadScript(ScriptEntity selectedScript){
+		this.selectedScript=selectedScript;
+	}
+
+	public void unloadScript(){
+		this.selectedScript=null;
+	}
+
+	public boolean currentScript(ScriptEntity script){
+		if(this.selectedScript==null)return false;
+		return this.selectedScript.getId()==script.getId();
+	}
+
+	public boolean hasScript(){
+		return this.selectedScript!=null;
+	}
+
+	private void cleanBean() {
+		this.interviewDate=null;
+
+		this.selectedScript=null;
+
+		if(this.interviewers!=null)
+			this.interviewers.clear();
+
+		if(this.selectedInterviewers!=null)
+			this.selectedInterviewers.clear();
+
+		if(this.submissions!=null)
+			this.submissions.clear();
 
 		this.submission=null;
 
-		this.interviews.clear();
+		if(this.interviews!=null)
+			this.interviews.clear();
 
 		this.interviewDate=null;
-		
+
+	}
+
+	public boolean canSubmit(){
+		return this.hasSelectedInterviewers()&&this.hasDate()&&this.conflicts.isEmpty();
+	}
+
+	public void buildConflicts(){
+		if(this.conflicts==null)
+			this.conflicts=new ArrayList<>();
+
+		this.conflicts.clear();
+
+		if(this.interviewEJB.candidateHasDateConflict(this.interviewDate, this.submission.getCandidate()))
+			this.conflicts.add("O candidato tem um conflito de agenda");			
+
+		for(UserEntity interviewer:this.selectedInterviewers)
+			if(this.interviewEJB.interviewerHasDateConflict(this.interviewDate, interviewer))
+				this.conflicts.add("O entrevistador "+interviewer.getFirstName()+" "+interviewer.getLastName()+" tem um conflito de agenda");
+	}
+
+	public List<String> getConflicts() {
+		return conflicts;
+	}
+
+	public void setConflicts(List<String> conflicts) {
+		this.conflicts = conflicts;
+	}
+
+	public boolean checkPosition(PositionEntity position){
+		if(this.position==null)return false;
+
+		return this.position.getId()==position.getId();
+	}
+
+	public void searchPositionsManagedByUser(long idM){
+		log.info("Searching for positions by manager");
+		log.debug("Id "+idM);
+		UserEntity manager = userEJB.find(idM);
+		if (manager != null && manager.getRoles().contains("MANAGER")) {
+			log.debug("Manager "+manager.getFirstName());
+			this.positions = positionEJB.findPositionsManagedByUser(manager);
+		} else log.error("No manager with id "+idM);
+	}
+
+	public void searchPositionsByKeywordAndManager(Long idM) {
+		log.info("Searching for positions of manager by keyword");
+		log.debug("Id "+idM);
+		UserEntity manager = userEJB.find(idM);
+		if ( (manager != null) && (manager.getRoles().contains("MANAGER")) ) {
+			log.debug("Manager "+manager.getFirstName());
+			String pattern = SearchPattern.preparePattern(keyword);
+			log.debug("Internal search string: "+pattern);
+			this.positions = positionEJB.findPositionsByKeywordAndManager(pattern,
+					manager);
+		} else log.error("No manager with id "+idM);
+	}
+
+	public List<PositionEntity> getPositions() {
+		return positions;
+	}
+
+	public void setPositions(List<PositionEntity> positions) {
+		this.positions = positions;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
 	}
 
 }
