2c7e4cf3471d5047f98a17bccbd4191f9ff0895e
renatadiasilva <renatadiasilva@gmail.com>
2015-10-02 15:40:47 +0100

    remoção cenas não usadas

--- Final_Project_EJB/src/main/java/pt/uc/dei/aor/pf/urlQueriesManagement/URLQueriesManagementInterface.java
+++
@@ -1,9 +0,0 @@
-package pt.uc.dei.aor.pf.urlQueriesManagement;
-
-import java.util.List;
-
-public interface URLQueriesManagementInterface {
-
-	public abstract List<String> authenticateCandidate(String email);
-
-}
